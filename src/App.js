021b1936594f87a815751c7fcd868a6a52e75269
Igor Rocha <igortice@gmail.com>
2020-01-04 20:21:58 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Init App</Text>
+    </View>
+  );
+};
+
+export default App;
