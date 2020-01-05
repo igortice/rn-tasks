d07ec131433300847c66815041fb84f52207fe0c
Igor Rocha <igortice@gmail.com>
2020-01-04 22:22:04 -0300

    init add nativebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import {Text, View} from 'react-native';
+import {Body, Container, Header, Title} from 'native-base';
+import {Col, Grid} from 'react-native-easy-grid';
 
 import React from 'react';
+import {StyleSheet} from 'react-native';
 
-const App = () => {
-  return (
-    <View>
-      <Text>Init App</Text>
-    </View>
-  );
-};
+const App = () => (
+  <Container>
+    <Header>
+      <Body>
+        <Title>Header</Title>
+      </Body>
+    </Header>
+    <Grid>
+      <Col style={styles.bg1} />
+      <Col style={styles.bg2} />
+    </Grid>
+  </Container>
+);
 
 export default App;
+
+const styles = StyleSheet.create({
+  bg1: {
+    backgroundColor: '#635DB7',
+  },
+  bg2: {
+    backgroundColor: '#00CE9F',
+  },
+});
